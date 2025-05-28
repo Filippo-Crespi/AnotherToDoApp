0434bbd59d42aa7ce11fb010503b62c5e5242552
Filippo-Crespi <crespifilippo123@gmail.com>
2025-05-28 18:00:59 +0200

    feat: move meta tags from script to template for improved SEO and mobile compatibility

--- app.vue
+++ app.vue
@@ -1,20 +1,17 @@
 <template>
+
+  <head>
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-title" content="ShareTunes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+    <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+  </head>
+
   <div>
     <NuxtPage />
   </div>
 </template>
 
 <script lang="ts">
-
-definePageMeta({
-  meta: [
-    { name: 'apple-mobile-web-app-capable', content: 'yes' },
-    { name: 'apple-mobile-web-app-title', content: 'AnotherToDoList' },
-    { name: 'apple-mobile-web-app-status-bar-style', content: 'default' }
-  ],
-  link: [
-    { rel: 'apple-touch-icon', href: '/icons/icon-192x192.png' }
-  ]
-});
 
 </script>
