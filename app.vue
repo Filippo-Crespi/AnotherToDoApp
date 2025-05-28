031d77958f83b66f14abf1339bfdd1008453052d
Filippo-Crespi <crespifilippo123@gmail.com>
2025-05-28 18:08:09 +0200

    refactor: rearrange head section in app.vue for better structure

--- app.vue
+++ app.vue
@@ -1,13 +1,12 @@
 <template>
-
-  <head>
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-title" content="ShareTunes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-    <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-  </head>
-
   <div>
+
+    <head>
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-title" content="ShareTunes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+    </head>
     <NuxtPage />
   </div>
 </template>
